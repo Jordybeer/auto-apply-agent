2fdec73dd44765e9f6a1663e52fcc3f4e5eb564b
jordybeer <contact@jordy.beer>
2026-04-02 14:51:39 +0200

    fix: darkMode uses data-theme attribute instead of class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import animatePlugin from 'tailwindcss-animate';
 
 const config: Config = {
-  darkMode: 'class',
+  darkMode: ['attribute', 'data-theme'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
